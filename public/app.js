fca069ad026d0f0a4adcbb43fae2ade2abf708be
Marie-hinrichs <hinimini209@gmail.com>
2023-05-28 18:50:15 +0200

    fancy background

--- public/app.js
+++ public/app.js
@@ -1,41 +1,62 @@
-module.exports = (io) => {
-  const players = [];
-  let tagger = null;
+// app.js
+const socket = io();
 
-  io.on("connection", (socket) => {
-    console.log("Neue Verbindung hergestellt:", socket.id);
+socket.on("init", (data) => {
+  const { players, tagger } = data;
+  for (const player of players) {
+    createPlayerElement(player, tagger);
+  }
+});
 
-    const player = {
-      id: socket.id,
-      x: Math.random() * 800,
-      y: Math.random() * 600,
-      color: "#" + ((Math.random() * 0xffffff) << 0).toString(16),
-    };
-    players.push(player);
+socket.on("update", (player) => {
+  const playerElement = document.getElementById(player.id);
+  if (playerElement) {
+    playerElement.style.left = `${player.x}px`;
+    playerElement.style.top = `${player.y}px`;
+  }
+});
 
-    socket.emit("init", { players, tagger });
+socket.on("playerDisconnected", (playerId) => {
+  const playerElement = document.getElementById(playerId);
+  if (playerElement) {
+    playerElement.remove();
+  }
+});
 
-    socket.on("move", (movement) => {
-      const { x, y } = movement;
-      player.x += x; // Anstatt die Position zu ersetzen, füge die Bewegung zur aktuellen Position hinzu
-      player.y += y;
-      io.emit("update", player);
-    });
+socket.on("taggerSelected", (taggerId) => {
+  const previousTaggerElement = document.querySelector(".tagger");
+  if (previousTaggerElement) {
+    previousTaggerElement.classList.remove("tagger");
+  }
+  const newTaggerElement = document.getElementById(taggerId);
+  if (newTaggerElement) {
+    newTaggerElement.classList.add("tagger");
+  }
+});
 
-    socket.on("disconnect", () => {
-      const index = players.findIndex((p) => p.id === socket.id);
-      if (index !== -1) {
-        players.splice(index, 1);
-        io.emit("playerDisconnected", socket.id);
-        if (tagger === socket.id) {
-          tagger = null;
-        }
-      }
-    });
+function createPlayerElement(player, tagger) {
+  const playerElement = document.createElement("div");
+  playerElement.id = player.id;
+  playerElement.className = "player";
+  playerElement.style.backgroundColor = player.color;
+  playerElement.style.left = `${player.x}px`;
+  playerElement.style.top = `${player.y}px`;
+  if (tagger === player.id) {
+    playerElement.classList.add("tagger");
+  }
+  document.getElementById("game").appendChild(playerElement);
+}
 
-    if (players.length >= 2 && !tagger) {
-      tagger = players[Math.floor(Math.random() * players.length)].id;
-      io.emit("taggerSelected", tagger);
-    }
-  });
-};
+document.addEventListener("keydown", (event) => {
+  let movement = { x: 0, y: 0 };
+  if (event.key === "ArrowUp") {
+    movement.y = -10;
+  } else if (event.key === "ArrowDown") {
+    movement.y = 10;
+  } else if (event.key === "ArrowLeft") {
+    movement.x = -10;
+  } else if (event.key === "ArrowRight") {
+    movement.x = 10;
+  }
+  socket.emit("move", movement);
+});
